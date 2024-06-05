5dd5c27b0a85fcc97aa9a60b117da423e94cd5ee
Yann Soubeyrand <yann.soubeyrand@camptocamp.com>
2024-06-05 16:23:16 +0200

    feat: add caddy module
    Signed-off-by: Yann Soubeyrand <yann.soubeyrand@camptocamp.com>

---
+++ caddy/main.go
@@ -0,0 +1,66 @@
+// Caddy
+//
+// Get a container or service running Caddy HTTP server to serve static content without TLS.
+
+// Copyright Camptocamp SA
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+package main
+
+const (
+	// Caddy container image registry
+	ImageRegistry string = "docker.io"
+	// Caddy container image repository
+	ImageRepository string = "caddy"
+	// Caddy container image tag
+	ImageTag string = "2.8.4"
+	// Caddy container image digest
+	ImageDigest string = "sha256:4718355ff1e2592290e49950f01fb1d4b75adb920a7695aedd94b6a4590a684b"
+)
+
+// Caddy
+type Caddy struct {
+	// +private
+	Directory *Directory
+}
+
+// Caddy constructor
+func New(
+	// Directory containing static content to serve
+	directory *Directory,
+) *Caddy {
+	caddy := &Caddy{
+		Directory: directory,
+	}
+
+	return caddy
+}
+
+// Get a Caddy container ready to serve the static content
+//
+// Static content is mounted under `/usr/share/caddy` and container exposes port 8080.
+func (caddy *Caddy) Container() *Container {
+	caddyfile := dag.CurrentModule().Source().File("Caddyfile")
+
+	container := dag.Container().
+		From(ImageRegistry+"/"+ImageRepository+":"+ImageTag+"@"+ImageDigest).
+		WithEntrypoint([]string{"sh", "-c"}).
+		WithExec([]string{"chown 65535:65535 /config/caddy"}).
+		WithExec([]string{"chown 65535:65535 /data/caddy"}).
+		WithEntrypoint([]string{"caddy"}).
+		WithDefaultArgs([]string{"run", "--config", "/etc/caddy/Caddyfile", "--adapter", "caddyfile"}).
+		WithFile("/etc/caddy/Caddyfile", caddyfile).
+		WithMountedDirectory("/usr/share/caddy", caddy.Directory).
+		WithUser("65535").
+		WithExposedPort(8080).
+		WithExec(nil)
+
+	return container
+}
+
+// Get a Caddy service serving the static content
+//
+// See `container()` for details.
+func (caddy *Caddy) Server() *Service {
+	return caddy.Container().AsService()
+}
