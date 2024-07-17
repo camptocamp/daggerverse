17e3fe1051c3d269b40ab5540c5d830dd7178e69
Yann Soubeyrand <yann.soubeyrand@camptocamp.com>
2024-07-17 12:45:08 +0200

    feat(sass): add support to get container images for other platforms
    Signed-off-by: Yann Soubeyrand <yann.soubeyrand@camptocamp.com>

--- sass/main.go
+++ sass/main.go
@@ -18,48 +18,46 @@ import (
 type Sass struct {
 	// +private
 	Version string
-	// +private
-	Platform dagger.Platform
 }
 
 // Sass constructor
 func New(
-	ctx context.Context,
 	// Sass version to get
 	version string,
-	// Platform to get Sass for
-	// +optional
-	platform dagger.Platform,
 ) (*Sass, error) {
-	if platform == "" {
-		defaultPlatform, err := dag.DefaultPlatform(ctx)
-
-		if err != nil {
-			return nil, fmt.Errorf("failed to get platform: %w", err)
-		}
-
-		platform = defaultPlatform
-	}
-
 	sass := &Sass{
-		Version:  version,
-		Platform: platform,
+		Version: version,
 	}
 
 	return sass, nil
 }
 
 // Get Sass binaries (Dart runtime and Sass snapshot)
-func (sass *Sass) Binaries() *dagger.Directory {
-	platform := strings.Split(string(sass.Platform), "/")
+func (sass *Sass) Binaries(
+	ctx context.Context,
+	// Platform to get Sass for
+	// +optional
+	platform dagger.Platform,
+) (*dagger.Directory, error) {
+	if platform == "" {
+		defaultPlatform, err := dag.DefaultPlatform(ctx)
 
-	os := platform[0]
+		if err != nil {
+			return nil, fmt.Errorf("failed to get platform: %s", err)
+		}
+
+		platform = defaultPlatform
+	}
+
+	platformElements := strings.Split(string(platform), "/")
+
+	os := platformElements[0]
 	arch := map[string]string{
 		"amd64": "x64",
 		"386":   "ia32",
 		"arm":   "arm",
 		"arm64": "arm64",
-	}[platform[1]]
+	}[platformElements[1]]
 
 	downloadURL := "https://github.com/sass/dart-sass/releases/download/" + sass.Version
 
@@ -71,73 +69,117 @@ func (sass *Sass) Binaries() *dagger.Directory {
 		WithMountedFile("sass.tar.gz", tarball).
 		WithExec([]string{"tar", "--extract", "--strip-components", "1", "--file", "sass.tar.gz"})
 
-	directory := dag.Directory().
+	binaries := dag.Directory().
 		WithFile("dart", container.File("src/dart")).
 		WithFile("sass.snapshot", container.File("src/sass.snapshot"))
 
-	return directory
+	return binaries, nil
 }
 
 // Get a root filesystem overlay with Sass
 func (sass *Sass) Overlay(
+	ctx context.Context,
+	// Platform to get Hugo for
+	// +optional
+	platform dagger.Platform,
 	// Filesystem prefix under which to install Sass
 	// +optional
 	prefix string,
-) *dagger.Directory {
+) (*dagger.Directory, error) {
 	if prefix == "" {
 		prefix = "/usr/local"
 	}
 
-	directory := dag.Directory().
+	binaries, err := sass.Binaries(ctx, platform)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to get Sass binaries: %s", err)
+	}
+
+	overlay := dag.Directory().
 		WithDirectory(prefix, dag.Directory().
-			WithDirectory("libexec/sass", sass.Binaries()).
+			WithDirectory("libexec/sass", binaries).
 			WithFile("bin/sass", dag.CurrentModule().Source().File("bin/sass"), dagger.DirectoryWithFileOpts{Permissions: 0o755}),
 		)
 
-	return directory
+	return overlay, nil
 }
 
 // Install Sass in a container
 func (sass *Sass) Installation(
+	ctx context.Context,
 	// Container in which to install Sass
 	container *dagger.Container,
-) *dagger.Container {
-	container = container.
-		WithDirectory("/", sass.Overlay(""))
+) (*dagger.Container, error) {
+	platform, err := container.Platform(ctx)
 
-	return container
+	if err != nil {
+		return nil, fmt.Errorf("failed to get container platform: %s", err)
+	}
+
+	overlay, err := sass.Overlay(ctx, platform, "")
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to get Sass overlay: %s", err)
+	}
+
+	container = container.
+		WithDirectory("/", overlay)
+
+	return container, nil
 }
 
 // Get a Sass container from a base container
 func (sass *Sass) Container(
+	ctx context.Context,
 	// Base container
 	container *dagger.Container,
-) *dagger.Container {
+) (*dagger.Container, error) {
+	container, err := sass.Installation(ctx, container)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to install Sass: %s", err)
+	}
+
 	container = container.
-		With(sass.Installation).
 		WithEntrypoint([]string{"sass"}).
 		WithoutDefaultArgs()
 
-	return container
+	return container, nil
 }
 
 // Get a Red Hat Universal Base Image container with Sass
-func (sass *Sass) RedhatContainer() *dagger.Container {
-	container := sass.Container(dag.Redhat().Container())
+func (sass *Sass) RedhatContainer(
+	ctx context.Context,
+	// Platform to get container for
+	// +optional
+	platform dagger.Platform,
+) (*dagger.Container, error) {
+	container := dag.Redhat().Container(dagger.RedhatContainerOpts{Platform: platform})
 
-	return container
+	return sass.Container(ctx, container)
 }
 
 // Get a Red Hat Minimal Universal Base Image container with Sass
-func (sass *Sass) RedhatMinimalContainer() *dagger.Container {
-	container := sass.Container(dag.Redhat().Minimal().Container())
+func (sass *Sass) RedhatMinimalContainer(
+	ctx context.Context,
+	// Platform to get container for
+	// +optional
+	platform dagger.Platform,
+) (*dagger.Container, error) {
+	container := dag.Redhat().Minimal().Container(dagger.RedhatMinimalContainerOpts{Platform: platform})
 
-	return container
+	return sass.Container(ctx, container)
 }
 
 // Get a Red Hat Micro Universal Base Image container with Sass
-func (sass *Sass) RedhatMicroContainer() *dagger.Container {
-	container := sass.Container(dag.Redhat().Micro().Container())
+func (sass *Sass) RedhatMicroContainer(
+	ctx context.Context,
+	// Platform to get container for
+	// +optional
+	platform dagger.Platform,
+) (*dagger.Container, error) {
+	container := dag.Redhat().Micro().Container(dagger.RedhatMicroContainerOpts{Platform: platform})
 
-	return container
+	return sass.Container(ctx, container)
 }
